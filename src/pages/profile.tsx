a88c214040378cdd55b18204ffaccdcb5d961c47
mo-ri-regen <moricontb@gmail.com>
2021-11-22 10:05:04 +0900

    🍱 プロフィールページ追加

---
+++ src/pages/profile.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import React from "react";
+import { Navigation } from "../component/navigation";
+const Profile: NextPage = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+    </div>
+  );
+};
+
+export default Profile;
