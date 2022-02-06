fa239564d935563bd928e39024c3445dae4db08b
mo-ri-regen <moricontb@gmail.com>
2022-02-06 21:04:20 +0900

    🔧Tailwind CSSのバージョンアップによる設定ファイルの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-  purge: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/component/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
